35cc4d61224dbc159b816cbebffa254476252af9
Adam Hooper <adam@adamhooper.com>
2013-01-21 17:35:59 -0500

    Add run.sh (wrapper for /usr/local/bin)

---
+++ manage/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Wrapper for manage.rb.
+#
+# Symlink this file into /usr/local/bin. It will follow the symlink to this
+# directory and execute the command you give.
+
+CONFIG_DIR=/opt/overview/config/manage
+DIR=$(dirname $(readlink -f $0))
+
+(cd $DIR && ./manage.rb $@)
