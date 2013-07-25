2ee286fb623fcb4deea0f786accb6707fccc1f34
Jonas Karlsson <thejonas@gmail.com>
2013-07-25 18:41:25 -0400

    Set correct security group

--- instances/searchindex_staging/searchindex_staging_aws_instance.rb
+++ instances/searchindex_staging/searchindex_staging_aws_instance.rb
@@ -4,6 +4,6 @@ require_relative '../worker/worker_aws_instance'
 
 class SearchindexStagingAwsInstance < WorkerAwsInstance
   def security_group
-    'worker-staging'
+    'searchindex-staging'
   end
 end
