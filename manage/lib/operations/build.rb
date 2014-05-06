565acdf3db05310fa9b83dd418beed927181f957
Adam Hooper <adam@adamhooper.com>
2014-05-06 09:47:11 -0400

    Document Operations::Build

--- manage/lib/operations/build.rb
+++ manage/lib/operations/build.rb
@@ -2,6 +2,30 @@ require 'digest'
 require 'fileutils'
 
 module Operations
+  # Derives a SourceArtifact from a Source and version
+  #
+  # Usage:
+  #
+  #     source = ... a Source ...
+  #     treeish = 'origin/master' # or a sha1 or a tag
+  #     build = Build.new(source, treeish)
+  #     source_artifact = build.run
+  #     source_artifact.sha         # 'a1b2c3d4e5f6....'
+  #     source_artifact.zip_file    # '/path/to/archive.zip'
+  #     source_artifact.md5sum_file # '/path/to/md5sum'
+  #     source_artifact.valid?      # should be true
+  #
+  # When you run, Build does this:
+  #
+  # 1. Runs `git archive` to generate a snapshot
+  # 2. Extracts the archive to a (temporary) build directory
+  # 3. Runs `source.build_commands` in order as shell commands
+  # 4. Copies _archive.zip_ (which build_commands must generate) to
+  #    `source_artifact.zip_path` (its permanent home).
+  # 5. Generates an md5sum of _archive.zip_ and puts it in
+  #    `source_artifact.md5sum_path`
+  # 6. Deletes the build directory
+  # 7. Deletes the git archive
   class Build
     def initialize(source, treeish)
       @source = source
