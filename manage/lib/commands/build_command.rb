3ef8ffb7a8f1759224dd1a6263fb725bec2dc15f
Adam Hooper <adam@adamhooper.com>
2014-05-12 14:15:26 -0400

    Print a status message on build

--- manage/lib/commands/build_command.rb
+++ manage/lib/commands/build_command.rb
@@ -1,6 +1,7 @@
 require_relative 'base'
 require_relative '../arguments/source_at_version'
 require_relative '../pipeline_command_runner'
+require_relative '../log'
 
 module Commands
   class BuildCommand < Base
@@ -10,7 +11,8 @@ module Commands
 
     def run(runner, source_at_version)
       pipeline = PipelineCommandRunner.new(runner)
-      pipeline.build(source_at_version.source, source_at_version.version)
+      artifact = pipeline.build(source_at_version.source, source_at_version.version)
+      $log.info('build') { "Built #{artifact}" }
     end
   end
 end
