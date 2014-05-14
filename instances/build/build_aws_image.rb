2c66e221aa4a30e6d4d0bf1b278bbf60518e7552
Adam Hooper <adam@adamhooper.com>
2014-05-14 11:34:07 -0400

    Make build image work

--- instances/build/build_aws_image.rb
+++ instances/build/build_aws_image.rb
@@ -4,10 +4,27 @@ require_relative '../common/aws_image'
 
 class BuildAwsImage < AwsImage
   def packages
-    super + [ 'openjdk-7-jre-headless', 'build-essential', 'nodejs', 'nodejs-dev', 'npm' ]
+    super + %W(
+      build-essential
+      nodejs
+      nodejs-dev
+      npm
+      openjdk-7-jdk
+      zip
+    )
   end
 
   def self.type_tag
     'build'
+  end
+
+  def run_ssh_commands(ssh)
+    super(ssh)
+
+    build_commands = [
+      'sudo update-alternatives --install /usr/bin/node node /usr/bin/nodejs 10'
+    ]
+
+    ssh.exec("(#{build_commands.join(' && ')})")
   end
 end
